ef6b1576810f60e18e85170398ecb434d2937a7c
DraxusTDEV <draxustienda@gmail.com>
2022-11-14 20:03:29 -0500

    finished html and CSS, begining Js

---
+++ interactive-rating-component-main/app.js
@@ -0,0 +1,21 @@
+
+
+const button = document.querySelector('button')
+
+
+
+const uno = document.querySelector('#uno');
+const dos = document.querySelector('#dos');
+const tres = document.querySelector('#tres');
+const cuatro = document.querySelector('#cuatro');
+const cinco = document.querySelector('#cinco');
+
+
+
+uno.addEventListener('click', () => {
+    console.log(uno);
+})
+
+
+
+
