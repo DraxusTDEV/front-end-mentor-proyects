add666f9bf4668d0241c82848e935f44aee621d3
DraxusTDEV <draxustienda@gmail.com>
2022-11-20 10:10:06 -0500

    finished pc view

--- interactive-rating-component-main/app.js
+++ interactive-rating-component-main/app.js
@@ -1,10 +1,91 @@
 
+const selection = document.querySelectorAll('h4');
 
-const button = document.querySelector('button')
+const uno = document.getElementById('uno');
+const dos = document.querySelector('#dos');
+const tres = document.querySelector('#tres');
+const cuatro = document.querySelector('#cuatro');
+const cinco = document.querySelector('#cinco');
+
+const option = document.querySelector('#option');
+
+const button = document.querySelector('button');
+
+const frontFace = document.querySelector('.front')
+const backFace = document.querySelector('.back')
+
+
+backFace.classList.add('back-face')
+
+selection.forEach(item => {
+    item.addEventListener('click', () => {
+        if(item === uno){
+            option.innerHTML = 1;
+        }
+        if(item === dos){
+            option.innerHTML = 2;
+        }
+        if(item === tres){
+            option.innerHTML = 3;
+        }
+        if(item === cuatro){
+            option.innerHTML = 4;
+        }
+        if(item === cinco){
+            option.innerHTML = 5;
+        }
+    })
+});
+
+button.addEventListener('click', () => {
+    frontFace.classList.add('front-face')
+    backFace.classList.remove('back-face')
+})
+
+
+    
 
 
 
-const uno = document.querySelector('#uno');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------
+
+/*const all = document.querySelectorAll('h4')
+
+var primero = 0;
+var x = 0;
+
+var value = (valor) => {
+    //console.log(valor);
+    return valor
+}
+
+console.log(value.value);
+
+
+
+
+
+const uno = document.getElementById('uno');
 const dos = document.querySelector('#dos');
 const tres = document.querySelector('#tres');
 const cuatro = document.querySelector('#cuatro');
@@ -12,10 +93,54 @@ const cinco = document.querySelector('#cinco');
 
 
 
-uno.addEventListener('click', () => {
-    console.log(uno);
-})
+    /*all.forEach(element => {
+        element.addEventListener('click', () => {
+            if (element === uno) {
+                primero = 1
+                //console.log(primero);  
+            }else if (element === dos) {
+                primero = 2
+                //console.log(primero);   
+            }else if (element === tres) {
+                primero = 3
+                //console.log(primero);   
+            }else if (element === cuatro) {
+                primero = 4
+                //console.log(primero);   
+            }else if (element === cinco) {
+                primero = 5
+                //console.log(primero);   
+            }
+        })
+    });
 
 
 
+    
+    all.forEach(element => {
+        element.addEventListener('click', () => {
+        if (element === uno) {
+            value(1)
+            //console.log(primero);   
+        }else if (element === dos) {
+            value(2)
+            //console.log(primero);   
+        }else if (element === tres) {
+            value(3)
+            //console.log(primero);   
+        }else if (element === cuatro) {
+            value(4)
+            //console.log(primero);   
+        }else if (element === cinco) {
+            value(5)
+            //console.log(primero);   
+        }
+    })
+});
 
+
+
+console.log( primero);
+
+ 
+*/
